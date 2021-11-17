73cc0e49947952c8a8995205e0151157dbc0b2cb
Fernando Freires de Carvalho <fernandofreires17@gmail.com>
2021-11-17 00:06:39 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello World</h1>
+  <h1>Hello World {{ name }}</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      name: 'cataline',
+    }
+  },
+})
 </script>
